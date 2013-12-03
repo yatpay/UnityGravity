49f7a0e3167d2484101c2b644abbe630b38d6304
yatpay <>
2013-12-02 23:05:06 -0500

    * CameraController.cs: Initial commit with unity scripts
    * Gravity.cs:
    * RocketController.cs:

---
+++ rocket_cargo_test/Assets/Scripts/Gravity.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Gravity : MonoBehaviour {
+	
+	public static float range = 6.0f;
+	
+	void FixedUpdate()
+	{
+		Collider[] cols = Physics.OverlapSphere(transform.position, range);
+		List<Rigidbody> rbs = new List<Rigidbody>();
+		
+		foreach(Collider c in cols)
+		{
+			if (c.gameObject.tag == "planet")
+			{
+				continue;
+			}
+			Rigidbody rb = c.attachedRigidbody;
+			if (rb != null && rb != rigidbody && !rbs.Contains(rb))
+			{
+				rbs.Add(rb);
+				Vector3 offset = transform.position - c.transform.position;
+				rb.AddForce( offset / offset.sqrMagnitude * rigidbody.mass);
+			}
+		}
+	}
+}
---
+++ rocket_cargo_test/Assets/Scripts/RocketController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketController : MonoBehaviour {
+
+	public float thrust = 10.0f;
+	public float rcs_thrust = 1.0f;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		float rotate = Input.GetAxis("Horizontal") * -rcs_thrust;
+
+		bool engine_on = Input.GetKey("space");
+
+		bool reset = Input.GetKey("r");
+
+		float added_thrust = 0.0f;
+		if (engine_on)
+		{
+			added_thrust = thrust;
+		}
+
+		Vector3 movement = new Vector3(0.0f, added_thrust, 0.0f);
+		rigidbody.AddRelativeForce (movement * Time.deltaTime);
+
+		rigidbody.AddRelativeTorque(0.0f, 0.0f, rotate * Time.deltaTime);
+
+		if(reset)
+		{
+			transform.position = new Vector3(0.0f, 2.8f, 0.0f);
+			rigidbody.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+
+		}
+
+	}
+}
